4bedefb46d598c3805aa308e962789337cca1245
Soukupis <joseph.soukup@outlook.com>
2020-02-12 10:27:43 +0100

    Commands(not working SetMax)

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Commands.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private int _max;
+        private int _number;
+        private Random _random;
+
+        public int Number
+        {
+            get => _number+1;
+            set
+            {
+                _number = value;
+                NotifyPropertyChanged();
+            }
+            
+        }
+        public int Max
+        {
+            get => _max;
+            set
+            {
+                _max = value;
+                Roll.RaiseCanExecuteChanged();
+                NotifyPropertyChanged();
+            }
+        }
+        public RelayCommand Roll { get; set; }
+        public ParametrizedRelayCommand SetMax { get; set; }
+
+        public MainViewModel()
+        {
+            _random = new Random();
+            Roll = new RelayCommand(
+                () => { Number = _random.Next(Max); },
+                () => { return (Max > 0 && Max <= 6); }
+            );
+            SetMax = new ParametrizedRelayCommand(
+                (parameter) =>
+                {
+                    if (Int32.TryParse(parameter as string, out int result))
+                    {
+                        Max = result;
+                    }
+                    if (Roll.CanExecute(null)) Roll.Execute(null);
+                    
+                },
+                () => { return true; }
+            );
+            Max = 6;
+            Number = _random.Next(Max);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void  NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ ViewModel/ParametrizedRelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.ViewModel
+{
+    class ParametrizedRelayCommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<bool> _canExecute;
+        public event EventHandler CanExecuteChanged;
+        public ParametrizedRelayCommand(Action<object> execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
